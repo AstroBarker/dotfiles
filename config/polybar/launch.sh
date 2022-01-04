f30a0aaea766312f92ab0b06c4ab73e8e2449c31
Brandon Barker <bbarker.py@protonmail.ch>
2022-01-04 11:07:12 -0500

    adjust polybar to work with multiple monitors

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,10 +3,16 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# Deal with multiple monitors
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
+
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch
-polybar main &
 
 echo "Bar launched..."
