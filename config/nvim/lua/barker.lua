ca9a0541e521ed2a794cda10a1a5927aa6ac07fe
Brandon Barker <bbarker.py@protonmail.ch>
2022-11-26 17:54:33 -0500

    updates to csp to avail depreciation

--- config/nvim/lua/barker.lua
+++ config/nvim/lua/barker.lua
@@ -1,5 +1,5 @@
 -- Builtin LSP config --
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local nvim_lsp = require "lspconfig"
 local lsp_status = require("lsp-status")
 
